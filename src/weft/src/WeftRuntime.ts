c0131c8335ccfe9d085e680ffac5e656286d5b56
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-06-08 11:37:21 +0200

    Start fixing weft runtime to work with new error mechanism

--- src/weft/src/WeftRuntime.ts
+++ src/weft/src/WeftRuntime.ts
@@ -3,11 +3,10 @@ import { expressionBounds } from "../../compiler/parse/Utils";
 
 import { Scanner } from "../../compiler/scan/Scanner";
 import { Parser } from "../../compiler/parse/Parser";
-import { renamer } from "../../compiler/rename/Renamer";
+import { renameStmt } from "../../compiler/rename/Renamer";
 import { TypeChecker } from "../../compiler/typecheck/Typechecker";
 import { Interpreter } from "../../compiler/Interpreter";
 
-import { ErrorReporter } from "../../compiler/parse/Reporter";
 import { makeEnv } from "../../compiler/environment";
 import { Evaluation } from "../../editor/console";
 import type { TypeInfo } from "../../compiler/typecheck/Annotations";
@@ -29,81 +28,56 @@ export class WeftRuntime {
   }
 
   async evaluate(code: string, offset = 0): Promise<EvaluationResults> {
-    let reporter = new ErrorReporter();
-
     let results: Evaluation[] = [];
     let miniLocations: Location[] | undefined;
 
     try {
       const scanner = new Scanner(code);
       const tokens = scanner.scanTokens();
-      const parser = new Parser(tokens, this.env.typeEnv, reporter);
+      const parser = new Parser(tokens, this.env.typeEnv);
       const stmts = parser.parse();
 
-      renamer(stmts, this.env.typeEnv, reporter);
+      const renamedStmts = stmts.map((stmt) =>
+        renameStmt(stmt, this.env.typeEnv)
+      );
 
-      let typedStmts: Stmt<TypeInfo>[] = [];
+      let typechecker = new TypeChecker(this.env);
 
-      if (!reporter.hasError) {
-        let typechecker = new TypeChecker(reporter, this.env);
+      let typedStmts = renamedStmts.map((stmt) => typechecker.check(stmt));
 
-        for (let stmt of stmts) {
-          let typedStmt = typechecker.check(stmt);
-          typedStmts.push(typedStmt);
-          // generateTypeDiagnostics(sub, expr).forEach((annotation) => {
-          //   if (annotation.severity === "error") {
-          //     reporter.error(annotation.from, annotation.to, annotation.message);
-          //   }
-          // });
-        }
+      const interpreter = new Interpreter(this.env.typeEnv);
+
+      let values: string[];
+      let miniLocations;
+      [values, miniLocations] = interpreter.interpret(
+        typedStmts,
+        this.evalCounter++
+      );
+
+      if (miniLocations) {
+        miniLocations = miniLocations.map(([id, { from, to }]) => [
+          id,
+          { from: from + offset, to: to + offset },
+        ]);
       }
 
-      if (reporter.hasError) {
-        results.push({
-          input: code,
-          success: false,
-          text:
-            "Error: " +
-            reporter.errors.map((error) => error.message).join("\n"),
-        });
-      } else {
-        const interpreter = new Interpreter(reporter, this.env.typeEnv);
-
-        let values: string[];
-        let miniLocations;
-        [values, miniLocations] = interpreter.interpret(
-          typedStmts,
-          this.evalCounter++
-        );
-
-        if (miniLocations) {
-          miniLocations = miniLocations.map(([id, { from, to }]) => [
-            id,
-            { from: from + offset, to: to + offset },
-          ]);
-        }
-
-        values.forEach((text, i) => {
-          if (text !== "") {
-            let { from, to } = expressionBounds(stmts[i].expression);
-            results.push({
-              input: code.slice(from, to),
-              success: true,
-              text,
-            });
-          }
-        });
-
-        if (reporter.hasError) {
+      values.forEach((text, i) => {
+        if (text !== "") {
+          let { from, to } = expressionBounds(stmts[i].expression);
           results.push({
-            input: code,
-            success: false,
-            text:
-              "Error: " +
-              reporter.errors.map((error) => error.message).join("\n"),
+            input: code.slice(from, to),
+            success: true,
+            text,
           });
         }
-      }
+      });
+
+      results.push({
+        input: code,
+        success: false,
+        text:
+          "Error: " + reporter.errors.map((error) => error.message).join("\n"),
+      });
     } catch (error) {
       if (error instanceof Error) {
         results.push({
