095493c433395f65e3340ead5fb6d6f599dce859
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-10-12 04:49:48 -0400

    Add some types to represent binding declarations

--- src/compiler/parse/AST/Stmt.ts
+++ src/compiler/parse/AST/Stmt.ts
@@ -1,4 +1,6 @@
+import { TypeScheme } from "../../typecheck/TypeScheme";
 import { Expr } from "./Expr";
+import { TypeNode } from "./TypeNode";
 
 export type Stmt<Extend extends Stmt.Extension = Stmt.Extension> =
   | Stmt.Expression<Extend>
@@ -6,14 +8,30 @@ export type Stmt<Extend extends Stmt.Extension = Stmt.Extension> =
 
 export namespace Stmt {
   export enum Is {
+    Annotation = "Type Annotation",
+    Binding = "Binding",
     Expression = "Expression",
     Error = "Error",
   }
 
   export type Extension = {
+    "Stmt.Annotation": object;
+    "Stmt.Binding": object;
     "Stmt.Expression": object;
     "Stmt.Error": object;
   } & Expr.Extension;
+
+  export type Annotation<Extend extends Extension = Extension> = {
+    is: Is.Annotation;
+    name: Expr.Variable<Extend>;
+    type: TypeNode.Qual;
+  };
+
+  export type Binding<Extend extends Extension = Extension> = {
+    is: Is.Binding;
+    pattern: Expr.Variable<Extend>; // Just plain patterns for now
+    expression: Expr<Extend>;
+  };
 
   export type Expression<Extend extends Extension = Extension> = {
     is: Is.Expression;
