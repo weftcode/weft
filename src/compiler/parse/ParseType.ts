c362bcbb4602c106e1ce72af7a4dd58d0a64cc4b
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-10-19 15:34:08 -0400

    Work on monadic type parser

--- src/compiler/parse/ParseType.ts
+++ src/compiler/parse/ParseType.ts
@@ -1,126 +1,140 @@
+import { TokenType } from "../scan/TokenType";
+import { State } from "../utils/State";
+import { TypeNode } from "./AST/TypeNode";
 
+import { Parse } from "./Parse";
 
+// Type Nodes
+export function qualifiedType(): Parse<TypeNode> {
+  return functionType().bind((maybeContext) =>
+    Parse.match(TokenType.DoubleArrow).bind((isQualified) =>
+      isQualified
+        ? functionType().map<TypeNode>((type) => ({
+            is: TypeNode.Is.Qual,
+            context: asContext(maybeContext),
+            type,
+          }))
+        : State.of(maybeContext)
+    )
+  );
+}
 
-  // Type Nodes
-  qualifiedType(): TypeNode {
-    let context = this.functionType();
+function asContext(node: TypeNode.Type): TypeNode.Context {
+  if (node.is === TypeNode.Is.Tuple) {
+    return {
+      ...node,
+      items: node.items.map((item) => asClassAssertion(item)),
+    };
+  } else {
+    return asClassAssertion(node);
+  }
+}
 
-    if (this.match(TokenType.DoubleArrow)) {
-      return {
-        is: TypeNode.Is.Qual,
-        context: this.asContext(context),
-        type: this.functionType(),
-      };
-    } else {
-      return context;
-    }
+function asClassAssertion(node: TypeNode.Type): TypeNode.ClassAssertion {
+  if (node.is !== TypeNode.Is.App) {
+    throw new Error(`Class assertion can't be of form: ${node.is}`);
   }
 
-  private asContext(node: TypeNode.Type): TypeNode.Context {
-    if (node.is === TypeNode.Is.Tuple) {
-      return {
-        ...node,
-        items: node.items.map((item) => this.asClassAssertion(item)),
-      };
-    } else {
-      return this.asClassAssertion(node);
+  const { left, right } = node;
+
+  if (left.is === TypeNode.Is.Const) {
+    if (right.is !== TypeNode.Is.Var) {
+      throw new Error(`Class assertion must be in head-normal form`);
     }
+
+    return { ...node, left, right };
+  } else {
+    return { ...node, left: asClassAssertion(left) };
   }
+}
 
-  private asClassAssertion(node: TypeNode.Type): TypeNode.ClassAssertion {
-    if (node.is !== TypeNode.Is.App) {
-      throw new Error(`Class assertion can't be of form: ${node.is}`);
-    }
+function functionType(): Parse<TypeNode.Type> {
+  return typeApplication().bind((left) =>
+    Parse.match(TokenType.Arrow).bind((hasArrow) =>
+      hasArrow
+        ? functionType().map((right) => ({
+            is: TypeNode.Is.Func,
+            left,
+            right,
+          }))
+        : State.of(left)
+    )
+  );
+}
 
-    const { left, right } = node;
-
-    if (left.is === TypeNode.Is.Const) {
-      if (right.is !== TypeNode.Is.Var) {
-        throw new Error(`Class assertion must be in head-normal form`);
-      }
-
-      return { ...node, left, right };
-    } else {
-      return { ...node, left: this.asClassAssertion(left) };
-    }
-  }
-
-  private functionType(): TypeNode.Type {
-    let left = this.typeApplication();
-
-    if (this.match(TokenType.Arrow)) {
-      let right = this.functionType();
-      return { is: TypeNode.Is.Func, left, right };
-    } else {
-      return left;
-    }
-  }
-
-  private typeApplication(): TypeNode.Type {
-    // If there's a type function identifier
-    let left = this.typeTerm();
-
+function typeApplication(): Parse<TypeNode.Type> {
+  // If there's a type function identifier
+  return typeTerm().bind((left) => {
     if (!left) {
       throw new Error("Missing type term.");
     }
 
-    let right: TypeNode.Type | null;
+    return;
+  });
 
-    while ((right = this.typeTerm())) {
-      left = { is: TypeNode.Is.App, left, right };
-    }
+  let right: TypeNode.Type | null;
 
-    return left;
+  while ((right = this.typeTerm())) {
+    left = { is: TypeNode.Is.App, left, right };
   }
 
-  private typeTerm(): TypeNode.Type | null {
-    if (this.match(TokenType.LeftBracket)) {
-      let type = this.functionType();
-      this.consume(
-        TokenType.RightBracket,
-        "Expected right bracket at end of list type."
-      );
-      return { is: TypeNode.Is.List, type };
-    } else if (this.match(TokenType.LeftParen)) {
-      let items: TypeNode.Type[] = [];
+  return left;
+}
 
-      while (!this.match(TokenType.RightParen)) {
-        if (this.isAtEnd()) {
-          // TODO: Maybe specialize this to different possibilities
-          throw new Error("Unterminated tuple type");
-        }
+function typeTermList(): Parse<TypeNode[]> {
+  return typeTerm().bind(term => {
+    ?? [];
+  })
+}
 
-        if (items.length > 0) {
-          this.consume(TokenType.Comma, "Expect ',' after tuple item");
-        }
+function typeTerm(): Parse<TypeNode.Type | null> {
+  if (this.match(TokenType.LeftBracket)) {
+    let type = this.functionType();
+    this.consume(
+      TokenType.RightBracket,
+      "Expected right bracket at end of list type."
+    );
+    return { is: TypeNode.Is.List, type };
+  } else if (this.match(TokenType.LeftParen)) {
+    let items: TypeNode.Type[] = [];
 
-        items.push(this.functionType());
+    while (!this.match(TokenType.RightParen)) {
+      if (this.isAtEnd()) {
+        // TODO: Maybe specialize this to different possibilities
+        throw new Error("Unterminated tuple type");
       }
 
-      switch (items.length) {
-        case 0:
-          return { is: TypeNode.Is.Unit };
-        case 1:
-          return { is: TypeNode.Is.Group, type: items[0] };
-        default:
-          return { is: TypeNode.Is.Tuple, items };
+      if (items.length > 0) {
+        this.consume(TokenType.Comma, "Expect ',' after tuple item");
       }
-    } else {
-      return this.typeIdentifier();
+
+      items.push(this.functionType());
+    }
+
+    switch (items.length) {
+      case 0:
+        return { is: TypeNode.Is.Unit };
+      case 1:
+        return { is: TypeNode.Is.Group, type: items[0] };
+      default:
+        return { is: TypeNode.Is.Tuple, items };
+    }
+  } else {
+    return this.typeIdentifier();
+  }
+}
+
+function typeIdentifier(): TypeNode.Type | null {
+  if (this.match(TokenType.Identifier)) {
+    let name = this.previous();
+    let { lexeme } = name;
+
+    if (lexeme[0] >= "a" && lexeme[0] <= "z") {
+      return { is: TypeNode.Is.Var, name };
+    } else if (lexeme[0] >= "A" && lexeme[0] <= "Z") {
+      return { is: TypeNode.Is.Const, name };
     }
   }
 
-  private typeIdentifier(): TypeNode.Type | null {
-    if (this.match(TokenType.Identifier)) {
-      let name = this.previous();
-      let { lexeme } = name;
-
-      if (lexeme[0] >= "a" && lexeme[0] <= "z") {
-        return { is: TypeNode.Is.Var, name };
-      } else if (lexeme[0] >= "A" && lexeme[0] <= "Z") {
-        return { is: TypeNode.Is.Const, name };
-      }
-    }
-
-    return null;
-  }
+  return null;
+}
